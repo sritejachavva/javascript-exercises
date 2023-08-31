6d588d9b9e94c8b32d07a2cff32d8d01ee9fdf39
Sriteja Chavva <sritejachavva@gmail.com>
2023-08-30 19:33:40 -0700

    first 2 lessons

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(input, reps) {
+    let finalString = '';
+    if (reps<0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < reps; i++) {
+        finalString=finalString+input;
+    }
+    return finalString;
 
 };
 
