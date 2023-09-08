d2800f997fa30622aa16b216af0b53e9edd61b60
Sriteja Chavva <sritejachavva@gmail.com>
2023-09-07 23:36:04 -0700

    remove Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal_array) {
+    let index=0;
+    new_array = arr;
+    for (let i = 0; i < removal_array.length; i++) {
+        index=0;
+        index = new_array.indexOf(removal_array[i]);
 
+        while(index > -1){
+            console.log(index);
+            new_array.splice(index,1);
+            index = new_array.indexOf(removal_array[i]);
+        }
+      }
+      
+    return new_array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// Read full array and removal numbers into 2nd array
+// create for loop with length of 2nd array to remove each elemnt from array
