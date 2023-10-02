7030df3da5101b6baaf953a315e9a6042fac8a19
Sriteja Chavva <sritejachavva@gmail.com>
2023-10-01 22:14:38 -0700

    add delete event

--- custom_js_project/shopping_list.js
+++ custom_js_project/shopping_list.js
@@ -2,13 +2,34 @@
 let add_btn = document.querySelector('button');
 let li_items = document.body.querySelector('ul');
  
+
+
 function readInput(){
 
     let tempvar =  document.getElementById('item').value;
     console.log(document.getElementById('item').value);
+    let temp_span = document.createElement("span");
+    let temp_button = document.createElement("button");
     let temp_li = document.createElement("li");
-    temp_li.textContent = tempvar
+    temp_span.textContent = tempvar;
+    temp_button.innerHTML = "Delete";
     li_items.appendChild(temp_li);
+    
+    
+    temp_li.appendChild(temp_span);
+    temp_li.appendChild(temp_button);
+    document.getElementById('item').value = '';
+
+
+    function deleteItem(){
+        //alert("HI");
+        temp_li.parentNode.removeChild(temp_li);
+    }
+
+    
+
+    temp_button.addEventListener('click',deleteItem)
+
 }
 
 add_btn.addEventListener('click',readInput);
