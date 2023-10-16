304bc5d9e525a5411a22cb5bdc150f73e54cf4c1
Sriteja Chavva <sritejachavva@gmail.com>
2023-10-15 21:36:18 -0700

    fildtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    /*
+    step1: if death_date is undefined, then populate with current year
+    step2: add age field to each element
+    step3: sort by age and return first element
+    */
+
+    let populateDeathYear = function(arr){
+        for (let i in arr){
+            if (arr[i].yearOfDeath === undefined){
+                arr[i].yearOfDeath = 2023; 
+            }
+        }
+        return arr;
+    }
+
+    let calculateAge = function(arr){
+        for (let i in arr){
+            arr[i].age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        }
+        return arr;
+    }
+
+    let sortByAge = function(a,b){
+        return b.age-a.age;
+    }
+
+    arr = populateDeathYear(arr);
+    arr = calculateAge(arr);
+
+    
+
+    arr = arr.sort(sortByAge);
+
+    return arr[0];
 
 };
 
